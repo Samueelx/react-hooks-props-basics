3f3ee9e1041d935bf987858f633371dce6391fae
Samueelx <gsammygitau@gmail.com>
2023-01-12 15:01:14 +0300

    add props to the BlogContent component.

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+function BlogContent({ articleText, isPublished, minutesRead}) {
+  if(isPublished !== true)
+  return null; /**Hide unpublished content */
+
+  return (
+    <div>
+      <h1>{articleText}</h1>
+      <p>A {minutesRead} Minutes Read</p>
+    </div>
+  );
 }
 
 export default BlogContent;
